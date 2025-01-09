ebb6182cba9dc08c1304733d256f6476a9c55ccf
Jemil_Dilan <143726038+jemil-dilan@users.noreply.github.com>
2025-01-09 09:44:39 +0100

    Add files via upload

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,11 +1,9 @@
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Etudiant {
-    private String nom;
-    private int age;
-    private double moyenne;
+     String nom;
+     int age;
+     double moyenne;
 
     public Etudiant(String nom, int age, double moyenne) {
         this.nom = nom;
@@ -13,40 +11,11 @@ class Etudiant {
         this.moyenne = moyenne;
     }
 
-    public int getAge() {
-        return age;
-    }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public double getMoyenne() {
-        return moyenne;
-    }
-
-    public void setMoyenne(double moyenne) {
-        this.moyenne = moyenne;
-    }
-
-    @Override
-    public String toString() {
-        return "Nom: '" + nom + '\'' +
-                ", Age: " + age +
-                ", Moyenne: " + moyenne;
-    }
 }
 public class Exercise_1 {
     public static void main(String[] args){
-        ArrayList<Etudiant> etudiants = new ArrayList<>();
+        List<Etudiant> etudiants = new ArrayList<>();
         etudiants.add(new Etudiant("Paul", 22, 9.99));
         etudiants.add(new Etudiant("Jean", 25, 11.33));
         etudiants.add(new Etudiant("Bryan", 19, 12.11));
@@ -54,20 +23,24 @@ public class Exercise_1 {
         etudiants.add(new Etudiant("James", 19, 13.5));
 
         System.out.println("Liste des Etudiants: ");
+        System.out.println("Nom \t\t\tAge \t\tMoyenne");
+        System.out.println("--- \t\t\t--- \t\t------- \n");
         for (Etudiant e : etudiants){
-            System.out.println(e);
+            System.out.println(e.nom + "\t\t\t" + e.age + "\t\t\t" + e.moyenne );
         }
 
-        Collections.sort(etudiants, new Comparator<Etudiant>() {
+        etudiants.sort(new Comparator<Etudiant>() {
             @Override
             public int compare(Etudiant o1, Etudiant o2) {
-                return Double.compare(o2.getMoyenne(), o1.getMoyenne());
+                return Double.compare(o2.moyenne, o1.moyenne);
             }
         });
 
         System.out.println("\n Etudiant trier par moyenne décroisant: ");
-        for(Etudiant e : etudiants){
-            System.out.println(e);
+        System.out.println("Nom \t\t\tAge \t\tMoyenne");
+        System.out.println("--- \t\t\t--- \t\t------- \n");
+        for (Etudiant e : etudiants){
+            System.out.println(e.nom + "\t\t\t" + e.age + "\t\t\t" + e.moyenne );
         }
 
         Scanner scanner = new Scanner(System.in);
@@ -76,9 +49,10 @@ public class Exercise_1 {
 
         boolean trouver = false;
         for(Etudiant e : etudiants){
-            if(Objects.equals(nomEtudiant, e.getNom())){
+            if(Objects.equals(nomEtudiant, e.nom)){
                 trouver = true;
-                System.out.println("Etudiant trouvé: " + e);
+                System.out.println("\nEtudiant trouvé: ");
+                System.out.println("Nom: " + e.nom + "\tAge: " + e.age + " \tMoyenne: " + e.moyenne );
                 break;
             }
         }
